2a0dbf487303cc520ab790a02a8bb373a748b048
SylvainSenechal <senechalsylvain@hotmail.com>
2020-12-28 16:04:51 +0100

    remove password + new data fiels

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+require('dotenv').config()
 const got = require('got')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
+const dbPassword = process.env.DB_PASSWORD
+const dbName = process.env.DB_NAME
 // app.use(bodyParser.json())
 
 const MongoClient = require('mongodb').MongoClient
 const ObjectId = require('mongodb').ObjectId
-// TODO change password
-const uri = "mongodb+srv://dbUser:coucou@cluster0.uznmv.mongodb.net/<dbUser>?retryWrites=true&w=majority"
-const client = new MongoClient(uri) // (uri, { useNewUrlParser: true })
+const uri = `mongodb+srv://${dbName}:${dbPassword}@cluster0.uznmv.mongodb.net/retryWrites=true&w=majority`
+const client = new MongoClient(uri)
 
 const connectDatabase = async () => {
 	try {
@@ -24,13 +26,14 @@ const connectDatabase = async () => {
 
 connectDatabase().then( () => {
 	console.log('okkkk')
-	scrapPages(28, 50)
+	scrapPages(10, 10)
 })
 
 
 
 app.get('/', (req, res) => {
 	console.log('ok')
+	res.send( 'ok')
 })
 
 app.use((req, res, next) => {
@@ -49,7 +52,7 @@ app.use((error, req, res, next) => {
 	})
 })
 
-
+// VOIR DATES TRANSFORMER EN NB
 const scrapPages = async (startPage, endPage) => {
 	// const db = req.app.locals.database
 
@@ -106,14 +109,18 @@ const extractNextMessage = (data, pageNumber) => {
 		idPost: '',
 		namePoster: '',
 		datePost: '',
+		day: '',
+		month: '', 
+		year: '',
 		timePost: '',
+		timeNumber: '',
 		pageNumber: pageNumber,
 	}
 
 	data = data.slice(data.search('messagetable'))
 
 	data = data.slice(data.search('<a name="t') + 10) // + 10 removes <a name="t
-	infos.idPost = data.slice(0, data.search('"></a>'))
+	infos.idPost = Number(data.slice(0, data.search('"></a>')))
 
 	data = data.slice(data.search('s2') + 4) // + 4 removes s2">
 	infos.namePoster = data.slice(0, data.search('</b>'))
@@ -123,6 +130,14 @@ const extractNextMessage = (data, pageNumber) => {
 
 	data = data.slice(data.search('à') + 7) // +7 removes à&nbsp;
 	infos.timePost = data.slice(0, 8)
+
+	const [day, month, year] = infos.datePost.split('-')
+	infos.day = Number(day)
+	infos.month = Number(month)
+	infos.year = Number(year)
+
+	const [hour, minute, second] = infos.timePost.split(':')
+	infos.timeNumber = Number(hour) * 3600 + Number(minute) * 60 + Number(second)
 
 	let endMessagePosition = data.search('clear: both')
 	if (endMessagePosition === - 1) {
